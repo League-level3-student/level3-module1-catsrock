3e31ca580d9e170828b39135ca34cbea80a0c924
11000783 <josephaguilar2022@gmail.com>
2018-11-25 14:34:28 -0800

    Worked on Hangman
    Had a lot of stuff in keyTyped for step 2 but when testing none of the system.out.println worked
    Fix bugs next week!

--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -1,13 +1,93 @@
 package _04_HangMan;
 
-import javax.swing.JOptionPane;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.ArrayList;
+import java.util.Stack;
 
-public class HangMan{
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class HangMan implements KeyListener {
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JLabel label = new JLabel();
+	Stack<String> words = new Stack<String>();
+	String c;
+
 	public static void main(String[] args) {
-		String answer=JOptionPane.showInputDialog("Give a number from 1-266 inclusive");
-		int num1=Integer.parseInt(answer);
-		Utilities.getTotalWordsInFile("dictionary.txt");
+		new HangMan();
 	}
-	
-	
+
+	public HangMan() {
+
+		String answer = JOptionPane.showInputDialog("Give a number from 1-266 inclusive");
+		Utilities.getTotalWordsInFile("dictionary.txt");
+		int num1 = Integer.parseInt(answer);
+		for (int i = 0; i < num1; i++) {
+			String s = Utilities.readRandomLineFromFile("dictionary.txt");
+			while (words.contains(s)) {
+				s = Utilities.readRandomLineFromFile("dictionary.txt");
+			}
+			words.push(s);
+		}
+		System.out.println(words.peek());
+		words.pop();
+
+		frame.add(panel);
+		panel.add(label);
+		for (int i = 0; i < words.peek().length(); i++) {
+			label.setText(label.getText() + "_");
+		}
+		frame.addKeyListener(this);
+		frame.setSize(100, 100);
+		frame.setVisible(true);
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		System.out.println("TEST");
+		c = "" + (char) e.getKeyCode();
+		System.out.println(c);
+		if (Character.isLetter(c.charAt(0))) {
+			// label.setText(label.getText()+c);
+			ArrayList<Integer> indicesFound = new ArrayList<Integer>();
+			int previous = 0;
+			for (int i = 0; i < words.peek().length(); i++) {
+				int newIndex = words.peek().indexOf(c, previous);
+				previous = newIndex;
+				if (indicesFound.contains(newIndex) || newIndex == -1) {
+					break;
+				}
+				indicesFound.add(newIndex);
+			}
+			System.out.println(indicesFound);
+			String getText = label.getText();
+			String newText = "";
+			int start = 0;
+			for (int i = 0; i < indicesFound.size(); i++) {
+				newText += getText.substring(start, indicesFound.get(i));
+				start = indicesFound.get(i);
+				newText += c;
+			}
+			newText += getText.substring(indicesFound.get(indicesFound.size() - 1), getText.length());
+			System.out.println(getText);
+			System.out.println(newText);
+			label.setText(newText);
+			//frame.pack();
+		}
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+	}
 }
